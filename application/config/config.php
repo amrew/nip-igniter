c828c24ca772fa9fa538b5ea78d9372339278577
Hanif Nur Amrullah <honeyf.amrew@gmail.com>
2014-10-10 06:16:46 +0700

    Remove unexpected semicolon

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://'.$_SERVER['HTTP_HOST'].'/nip-igniter/';;
+$config['base_url']	= 'http://'.$_SERVER['HTTP_HOST'].'/nip-igniter/';
 
 /*
 |--------------------------------------------------------------------------
