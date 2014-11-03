bcd072b14be314d4a1f7046239b2e7d5f1fe38bf
Hanif Nur Amrullah <honeyf.amrew@gmail.com>
2014-11-04 04:53:16 +0700

    add report libraries

---
+++ application/libraries/dompdflib.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+require_once 'dompdf/dompdf_config.inc.php';
+
+class Dompdflib{
+
+	public $CI;
+
+	public function __construct(){
+		$this->CI =& get_instance();
+	}
+
+	public function generate($view, $fileName, $orientation = 'landscape'){
+		$dompdfx = new DOMPDF();
+		$dompdfx->set_paper('A4', $orientation);
+		$dompdfx->load_html($view);
+		$dompdfx->render();
+		$dompdfx->stream($fileName);
+	}
+}
